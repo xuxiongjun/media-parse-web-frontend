020e3c65619464faa5466b22727039dd8f9cb7ea
xuxiongjun <13169190736@163.com>
2026-09-17 14:41:57 +0800

    refactor(router): 路由组件改为懒加载
    将路由配置中的组件导入方式从静态导入改为动态导入，
    实现组件的懒加载以优化应用性能和减少初始包大小。
    
    - 将 HomeView 和 RemoveWatermarkView 改为异步加载
    - 使用 import() 函数进行代码分割

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import RemoveWatermarkView from '../views/RemoveWatermarkView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'parse', component: HomeView },
-    { path: '/remove-watermark', name: 'remove-watermark', component: RemoveWatermarkView }
+    {
+      path: '/',
+      name: 'parse',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/remove-watermark',
+      name: 'remove-watermark',
+      component: () => import('../views/RemoveWatermarkView.vue')
+    }
   ],
   scrollBehavior() {
     return { top: 0 }
